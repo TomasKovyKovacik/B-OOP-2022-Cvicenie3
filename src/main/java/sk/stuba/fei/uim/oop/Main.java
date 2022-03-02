1d6faa0a68f810908d29c7ea189339470bf666c7
TomasKovyKovacik <tomas_kovacik@stuba.sk>
2022-03-02 15:27:25 +0100

    triedy a modifikatory

--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -1,7 +1,19 @@
 package sk.stuba.fei.uim.oop;
 
+import sk.stuba.fei.uim.oop.auto.Auto;
+
 public class Main {
     public static void main(String[] args) {
+        Auto skoda = new Auto();
+        Auto bmw = new Auto();
 
+        skoda.jedna = 1;
+        bmw.jedna = 2;
+
+        if (skoda.jedna == bmw.jedna) {
+            System.out.println("rovnake");
+        } else {
+            System.out.println("rozne");
+        }
     }
 }
