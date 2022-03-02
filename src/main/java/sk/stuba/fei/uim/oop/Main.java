475c1cb86aa7366b0a703323fea587bd69f34deb
TomasKovyKovacik <tomas_kovacik@stuba.sk>
2022-03-02 16:26:33 +0100

    scanner random, z klavesnice , keyboard input

--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -1,21 +1,14 @@
 package sk.stuba.fei.uim.oop;
 
 import sk.stuba.fei.uim.oop.auto.Auto;
+import sk.stuba.fei.uim.oop.utility.KeyboardInput;
+import sk.stuba.fei.uim.oop.utility.ZKlavesnice;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Auto skoda = new Auto();
-        Auto vw = new Auto(50);
-
-        System.out.println("skoda");
-        System.out.println(skoda.stav());
-        skoda.jazdi(100);
-        System.out.println(skoda.stav());
-
-        System.out.println("vw");
-        System.out.println(vw.stav());
-        vw.jazdi(100);
-        System.out.println(vw.stav());
-
+        Auto auto = new Auto();
+        auto.nacitaj();
     }
 }
--- src/main/java/sk/stuba/fei/uim/oop/auto/Auto.java
+++ src/main/java/sk/stuba/fei/uim/oop/auto/Auto.java
@@ -1,62 +1,22 @@
 package sk.stuba.fei.uim.oop.auto;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Auto {
-    public static final double PALIVO_NA_KM = 0.2;
-    public static final double KAPACITA_NADRZE = 100;
-    private double kapacitaNadrze;
-    private double stavNadrze;
-    private boolean ojazdene;
+    private Scanner scanner;
+    private Random random;
 
     public Auto() {
-        this(KAPACITA_NADRZE);
+        this.scanner = new Scanner(System.in);
+        this.random = new Random();
     }
 
-    public Auto(double kapacitaNadrze) {
-        this.ojazdene = true;
-        this.stavNadrze = 50;
-        this.kapacitaNadrze = kapacitaNadrze;
-    }
+    public void nacitaj() {
+        System.out.println("zadaj string:");
+        String meno = scanner.nextLine();
+        System.out.println("Nacital som " + meno);
 
-    public void jazdi(double vzdialenost) {
-        this.ojazdene = true;
-        stavNadrze -= vzdialenost * PALIVO_NA_KM;
-        if (stavNadrze < 0) {
-            stavNadrze = 0;
-        }
-    }
-
-    public String stav() {
-        String vysledok;
-        if (this.ojazdene) {
-            vysledok = "Auto je ojazdene";
-        } else {
-            vysledok = "Auto je neojazdene";
-        }
-        vysledok += " * stav nadrze: " + stavNadrze + "/" + kapacitaNadrze;
-        return vysledok;
-    }
-
-    public double getKapacitaNadrze() {
-        return kapacitaNadrze;
-    }
-
-    public void setKapacitaNadrze(double kapacitaNadrze) {
-        this.kapacitaNadrze = kapacitaNadrze;
-    }
-
-    public double getStavNadrze() {
-        return stavNadrze;
-    }
-
-    public void setStavNadrze(double stavNadrze) {
-        this.stavNadrze = stavNadrze;
-    }
-
-    public boolean isOjazdene() {
-        return ojazdene;
-    }
-
-    public void setOjazdene(boolean ojazdene) {
-        this.ojazdene = ojazdene;
+        System.out.println("vygeneroval som nahodne cislo od 0 do 1000" + random.nextInt(1001));
     }
 }
